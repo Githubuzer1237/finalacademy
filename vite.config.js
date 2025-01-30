36cdc9344bb6db1286c7aa9b0f5fdb1639a2908b
Azim <158813082+Githubuzer1237@users.noreply.github.com>
2025-01-30 13:14:57 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
 
-
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       external: [], // Отключаем внешние модули
+    },
+  },
+  resolve: {
+    alias: {
+      gsap: "gsap/dist/gsap.js", // Указываем Vite, как правильно найти gsap
     },
   },
 });
